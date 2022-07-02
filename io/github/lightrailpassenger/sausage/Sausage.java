125285559a46020bc0903b8d7984d0f4d8f21be6
Tony Chow <tonychow.0929@icloud.com>
2022-07-02 18:33:43 +0800

    Set up look and feel

--- io/github/lightrailpassenger/sausage/Sausage.java
+++ io/github/lightrailpassenger/sausage/Sausage.java
@@ -1,6 +1,9 @@
 package io.github.lightrailpassenger.sausage;
 
+import java.awt.Font;
+import java.util.Map;
 import javax.swing.SwingUtilities;
+import javax.swing.UIManager;
 
 import static io.github.lightrailpassenger.sausage.constants.SausageConstants.*;
 
@@ -9,6 +12,28 @@ public class Sausage {
         SwingUtilities.invokeLater(new Runnable() {
             @Override
             public void run() {
+                try {
+                    String lookAndFeel = UIManager.getCrossPlatformLookAndFeelClassName();
+
+                    UIManager.setLookAndFeel(lookAndFeel);
+
+                    Map<Object, Object> lookAndFeelDefaults = UIManager.getLookAndFeelDefaults();
+
+                    for (Map.Entry<Object, Object> entry: lookAndFeelDefaults.entrySet()) {
+                        String key = entry.getKey().toString();
+
+                        if (key.endsWith(".font")) {
+                            Object value = lookAndFeelDefaults.get(key);
+
+                            if (value instanceof Font) {
+                                lookAndFeelDefaults.put(key, ((Font)value).deriveFont(Font.PLAIN, DEFAULT_FONT_SIZE));
+                            }
+                        }
+                    }
+                } catch (Exception ex) {
+                    // pass
+                }
+
                 SausageFrame frame = new SausageFrame();
                 PreferenceStore.getInstance().addFrame(frame);
 
--- io/github/lightrailpassenger/sausage/constants/SausageConstants.java
+++ io/github/lightrailpassenger/sausage/constants/SausageConstants.java
@@ -3,6 +3,7 @@ package io.github.lightrailpassenger.sausage.constants;
 import java.awt.Dimension;
 
 public interface SausageConstants {
+    public static final float DEFAULT_FONT_SIZE = 14f;
     public static final Dimension SAUSAGE_FRAME_DEFAULT_DIMENSION = new Dimension(800, 600);
     public static final String SAUSAGE_FRAME_TITLE = "Sausage";
 }
